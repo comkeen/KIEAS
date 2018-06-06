2d843bc9dc60e4e6e63d9b9278aff9c29a5449ba
comkeen <comkeen4@gmail.com>
2018-06-06 14:30:06 +0900

    update 경보표출 시퀀스다이어그램 uml

--- IEAS_K/src/kr/or/kpew/kieas/main/AlertSystemMain.java
+++ IEAS_K/src/kr/or/kpew/kieas/main/AlertSystemMain.java
@@ -2,8 +2,10 @@ package kr.or.kpew.kieas.main;
 
 import kr.or.kpew.kieas.alertsystem.AlertSystemManager;
 import kr.or.kpew.kieas.common.AlertSystemProfile;
+import kr.or.kpew.kieas.common.IssuerProfile;
 import kr.or.kpew.kieas.common.Profile;
 import kr.or.kpew.kieas.common.Profile.AlertSystemType;
+import kr.or.kpew.kieas.main.IntegratedAlertSystemMain.TransmitterType;
 import kr.or.kpew.kieas.network.jms.AlertSystemTransmitter;
 
 
@@ -11,7 +13,16 @@ public class AlertSystemMain
 {
 	public AlertSystemMain()
 	{
-//		new AlertSystemManager(new AlertSystemTransmitter(), new AlertSystemProfile("townbroadcast085@korea.kr", "경상남도", AlertSystemType.CivelDefense), new Profile("maingateway", "국민안전처"));
+		//게이트웨이 프로파일: 게이트웨이를 생성하기 위해 필요(식별자,이름)
+		Profile gatewayProfile = new Profile("maingateway", "국민안전처");
+		//발령대 프로파일: 발령대를 생성하기 위해 필요(식별자,이름)
+		IssuerProfile issuerProfile = new IssuerProfile("civilalerter", "민방위");
+		//경보시스템 프로파일: 수신기를 생성하기 위해 필요(식별자,지역,경보시스템타입)
+		AlertSystemProfile alertSystemProfile = new AlertSystemProfile("townbroadcast085", "경상남도", AlertSystemType.LocalBroadcasting);
+		alertSystemProfile.setLanguage("us-EN");
+		alertSystemProfile.setGeoCode("1100000000");
+		alertSystemProfile.setCapability("그림");
+		new AlertSystemManager(new AlertSystemTransmitter(), alertSystemProfile, gatewayProfile);
 	}
 	
 	public static void main(String[] args)
